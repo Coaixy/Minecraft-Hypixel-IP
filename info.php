212d5d20169e81413561c290467e023fa28b6d63
Yxiaoc <3045831167@qq.com>
2020-05-03 17:08:57 +0800

    Update info.php

--- info.php
+++ info.php
@@ -1,18 +1,25 @@
 <?php
+/**
+ * 网站主要信息区域
+ * 全局方法，全局常量
+ */
 // 数据库信息填写
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 header("Content-Type:text/html;charset=utf-8");
 define("sql_host", "127.0.0.1"); //数据库地址
 define("sql_port", 3306); //数据库端口
-define("sql_name", "NetWave"); //数据库名
+define("sql_name", ""); //数据库名
 define("sql_usr", ""); //数据库用户名
 define("sql_pwd", ""); //数据库密码
 define("salt", "nacl");
 //服务器信息
-define('host', '113.31.107.130');
+define('host', '');
 define('port', 22);
 define('usr', 'root');
-define('pwd', '(3*He[4#%Tgq');
+define('pwd', '');
+/**
+ * 分别是五个等级的到期和启用命令
+ */
 define('cmd1', 'iptables -A INPUT -s [ip] -m limit --limit 168/sec -p tcp --dport 39253 -j ACCEPT && service iptables save');
 define('cmd2', 'iptables -A INPUT -s [ip] -p tcp --dport 39253 -j ACCEPT && service iptables save');
 define('cmd3', 'iptables -A INPUT -s [ip] -p tcp --dport 39254 -j ACCEPT && service iptables save');
@@ -23,6 +30,13 @@ define('cmdd2', 'iptables -D INPUT -s [ip] -p tcp --dport 39253 -j ACCEPT && ser
 define('cmdd3', 'iptables -D INPUT -s [ip] -p tcp --dport 39254 -j ACCEPT && service iptables save');
 define('cmdd4', 'iptables -D INPUT -s [ip] -m limit --limit 160/sec -p tcp --dport 39253 -j ACCEPT && iptables -D INPUT -s [ip] -m limit --limit 160/sec -p tcp --dport 39254 -j ACCEPT && service iptables save');
 define('cmdd5', 'iptables -D INPUT -s [ip] -p tcp --dport 39253 -j ACCEPT && iptables -D INPUT -s [ip] -p tcp --dport 39254 -j ACCEPT && service iptables save');
+/**
+ * [sendCMDD description]
+ * @param  [type] $level [description]
+ * @param  [type] $ip    [description]
+ * @return [type]        [description]
+ * 根据等级执行命令
+ */
 function sendCMDD($level, $ip) {
     //echo str_replace('[ip]', $ip,constant("cmd".$level));
     if (!function_exists("ssh2_connect")) die("Error: SSH2 does not exist on you're server");
@@ -45,6 +59,12 @@ function sendCMDD($level, $ip) {
         }
     }
 }
+/**
+ * [str_check description]
+ * @param  [type] $value [description]
+ * @return [type]        [description]
+ * 过滤危险字符串
+ */
 function str_check($value) {
     if (!get_magic_quotes_gpc()) {
         // 进行过滤
