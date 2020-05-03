51e1eb22cda8b2f75b896a041458d9deebe94573
Yxiaoc <3045831167@qq.com>
2020-05-03 17:07:43 +0800

    Update user.php

--- user.php
+++ user.php
@@ -1,13 +1,14 @@
 <?php
 require 'info.php';
 $usr = $_REQUEST['usr'];
-//$usr = mysql_real_escape_string($usr);
 $pwd = $_REQUEST['pwd'];
 $ypwd = $pwd;
-check_param($usr);
-check_param($pwd);
+/**
+ * 过滤危险字符
+ */
+$usr = str_check($usr);
+$pwd = str_check($pwd);
 $conn = new mysqli(constant("sql_host"), constant("sql_usr"), constant("sql_pwd"), constant("sql_name"), constant("sql_port"));
-// var_dump($conn);
 if (!$conn->connect_error) {
     $sql_error = true;
 } else {
@@ -15,65 +16,54 @@ if (!$conn->connect_error) {
     $conn->close();
     die("数据库连接失败");
 }
-//$usr = mysql_real_escape_string($usr);
 $ip = NULL;
-//执行数据库命令
-//UPDATE user SET D = 1
-$cmd = "UPDATE user SET D = 1 WHERE `dateD` < now() AND D=0";
+$cmd = "UPDATE user SET D = 1 WHERE `dateD` < now() AND D=0";//将过期的用户全部切换状态
 mysqli_query($conn, $cmd);
 $cmd1 = "SELECT * FROM user WHERE D=1";
 $result = mysqli_query($conn, $cmd1);
 while ($row2 = mysqli_fetch_array($result)) {
-    //var_dump($row2);
     echo $row2['levelD'] . $row2['ip'];
     sendCMDD($row2['levelD'], getIP());
     sendCMDD($row2['levelD'], $row2['ip']);
     $usr = $row2['usr'];
     $cmd23 = "UPDATE user SET D = 2 WHERE usr = '$usr' ";
     mysqli_query($conn, $cmd23);
-}
+}//向服务器发送删除命令
 $cmd = "SELECT * FROM user WHERE usr='$usr'";
 $result = mysqli_query($conn, $cmd);
 if (!$result) {
     die(mysqli_error($conn));
 }
 $row = mysqli_fetch_array($result);
-//var_dump($row);
 if ($row != NULL) {
     $pwd = md5(constant("salt") . $pwd);
     if ($pwd == $row['pwd']) {
-        //var_dump($row);
         $ip = $row['ip'];
-        //echo "$ip";
         $group = $row['levelD'];
         $date = $row['dateD'];
         $qq = $row['QQ'];
         $h = $row['h'];
         $L = $row['L'];
         $D = $row['D'];
-        //$zero1=date("y-m-d h:i:s");
-        //$zero2=$row['dateD'];
+        //获取用户的属性
         if ($D == 1 || $D == 2) {
-            //sendCMDD($group,$ip);
-            //mysqli_query($conn,"UPDATE user SET D = 1 WHERE usr = '$usr' ");
             die("你的使用已到期 或 您的账户疑似共享已被管理员封禁。请查看邮箱");
         }
-        $rip = getIP();
+        $rip = getIP(); //获取访问者IP
         if (isset($_REQUEST['rip'])) {
             $zero1 = date("y-m-d h:i:s");
             $zero2 = $row['L'];
-            //echo strtotime($zero1);
-            //echo strtotime("+600 seconds",$zero2);
             if (strtotime($zero1) < strtotime("+600 seconds", strtotime($zero2))) {
                 die("宝贝，你再等等，我们得冷却十分钟才能改一次哦~");
             } else {
                 $cmd = "UPDATE user SET L = '$zero1' WHERE usr = '$usr' ";
                 mysqli_query($conn, $cmd);
             }
+            //冷却时间判定，避免爆破 限制时间10分钟
             if ($zero2 == "") {
                 $cmd = "UPDATE user SET L = '$zero1' WHERE usr = '$usr' ";
                 mysqli_query($conn, $cmd);
-            }
+            }//不存在则写入
             $rip = $_REQUEST['rip'];
             $cmd = "UPDATE user SET ip = '$rip' WHERE usr = '$usr' ";
             if (mysqli_query($conn, $cmd)) {
@@ -82,18 +72,15 @@ if ($row != NULL) {
                 sendCMD($group, $rip);
                 $a = 0;
                 foreach (explode("-", $h) as $var) {
-                    //echo "$var";
                     if ($var == $rip) {
                         $a = 1;
                     }
                 }
-                //$cmd="UPDATE user SET h = '$text' WHERE usr = '$usr' ";
-                //mysqli_query($conn, $cmd);
-                //print_r(explode("-",$h));
                 if ($a == 0 || $ip == "" || $h == "") {
                     $cmd = "UPDATE user SET h = '$text' WHERE usr = '$usr' ";
                     mysqli_query($conn, $cmd);
                 }
+                //历史IP已记录则不再次记录
             } else {
                 die("账号或密码错误");
             }
@@ -104,6 +91,10 @@ if ($row != NULL) {
 } else {
     die(mysqli_error($conn));
 }
+/**
+ * [getIP description]
+ * @return [type] [description]
+ */
 function getIP() {
     if (@$_SERVER["HTTP_X_FORWARDED_FOR"]) $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
     else if (@$_SERVER["HTTP_CLIENT_IP"]) $ip = $_SERVER["HTTP_CLIENT_IP"];
@@ -114,8 +105,12 @@ function getIP() {
     else $ip = "Unknown";
     return $ip;
 }
+/**
+ * [check_param description]
+ * @param  [type] $value [description]
+ * @return [type]        [description]
+ */
 function check_param($value = null) {
-    #  select|insert|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile
     $str = 'select|insert|and|or|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile';
     if (!$value) {
         exit('没有参数！');
