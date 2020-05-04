d17c428ebb33ae3b5ac8053f1b8d615ae77ff047
Coaixy <3045831167@qq.com>
2020-05-04 23:14:02 +0800

    Update keys.php

--- keys.php
+++ keys.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * 密钥生成器  格式 *****/keys.php?key=【密码】&num=【数量】&level=【级别】&dqtime=【时长】
- */
+ * 密钥生成器  格式 keys.php?key=【密码】&num=【数量】&level=【级别】&dqtime=【时长】
+**/
 require 'info.php';
 $key = $_REQUEST['key'];
 $num = $_REQUEST['num'];
@@ -11,7 +11,6 @@ if ($key != "NetWave.lmfuture.czy") {
     die();
 }
 $conn = new mysqli(constant("sql_host"), constant("sql_usr"), constant("sql_pwd"), constant("sql_name"), constant("sql_port"));
-// var_dump($conn);
 if (!$conn->connect_error) {
     $sql_error = true;
 } else {
@@ -20,14 +19,11 @@ if (!$conn->connect_error) {
     die("数据库连接失败");
 }
 $time = time();
-// var_dump($time);
-// echo "$";
+//利用时间戳进行卡密生成
 for ($i = 0;$i < $num;$i++) {
     $my = md5($time . $i);
     $cmd = "INSERT INTO my (mm,dateD,groupD) VALUES ('$my','$dqtime','$level')";
     var_dump(mysqli_query($conn, $cmd));
     echo "$my";
-    //var_dump(mysqli_error($conn));
-    
 }
 ?>
